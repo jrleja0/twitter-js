c112849e2c0ad7a1e8bc24ddb3a0ab8c51dc72a2
David R Lowry Lowry <ross@Davids-MacBook-Pro.local>
2017-04-25 10:52:40 -0400

    html now logging in console

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const app = express();
+const volleyball = require('volleyball')
+const nunjucks = require('nunjucks')
 
 var PORT = 3000;
 app.listen(PORT, function() {
   console.log(`Server listening on ${PORT}`);
 });
 
+app.use(volleyball)
+
 app.get('/', function (req, res) {
+	console.log('get /')
   res.send('Hello World!')
 });
+
+app.get('/is-anybody-in-there/yessomeones', function (req, res) {
+	console.log('get /is-anybody-in-there')
+  res.send('Hello World!')
+});
+
+app.post('/modernism', function (req, res) {
+	console.log('POST /modernism')
+});
+
+
+app.get('/news', function (req, res) {
+	res.send('you are on the news page. woop!')
+});
+
+var locals = {
+	title: 'An Example',
+	people: [
+		{ name: 'Gandalf'},
+		{ name: 'Frodo'},
+		{ name: 'Hermione'}
+	]
+};
+
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+	console.log(output);
+})
